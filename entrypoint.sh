072b9782f2bee4f83e2a2dd1068cd9ef364a6b77
fusionx3 <seriousblank@gmail.com>
2022-07-15 15:56:44 +0400

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -44,7 +44,7 @@ EOF
 # - Sync using our dedicated profile and suppress verbose messages.
 #   All other flags are optional via the `args:` directive.
 sh -c "yarn" \
-&& sh -c "yarn build:production" \
+&& sh -c "GENERATE_SOURCEMAP=false REACT_APP_ENV=production yarn build" \
 && sh -c "aws s3 sync ${SOURCE_DIR:-public} s3://${AWS_S3_BUCKET}/${DEST_DIR} \
               --profile react-deploy-to-s3-action \
               --no-progress \
